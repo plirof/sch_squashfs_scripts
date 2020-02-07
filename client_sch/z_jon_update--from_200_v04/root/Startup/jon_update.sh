81d1ddadda0fdeaf25b9b0401801132c01fd2515
aaa <aaa@aaa.com>
2020-02-07 09:41:47 +0200

    200107 - added iptables command in case of server IP change v04c

--- client_sch/z_jon_update--from_200_v04/root/Startup/jon_update.sh
+++ client_sch/z_jon_update--from_200_v04/root/Startup/jon_update.sh
@@ -1,3 +1,7 @@
+# If you change server IP, use the following iptables command for redirect (OLD_SERVER_IP is 192.168.1.200)
+# iptables -t nat -A OUTPUT -d OLD_SERVER_IP -j DNAT --to-destination NEW_SERVER_IP
+# iptables -t nat -A OUTPUT -d 192.168.1.200 -j DNAT --to-destination 192.168.1.171
+#you can create a snapshot of the current ruleset with iptables-save > /path/to/file and restore it later with iptables-restore < /path/to/file
 cd /tmp
 rm /tmp/john_downloads.sh
 wget http://192.168.1.200/uploads/john_downloads.sh
