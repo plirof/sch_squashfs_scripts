40933768eedc8fc6814a599c5232a784006ad8ea
aaa <aaa@aaa.com>
2020-11-06 11:43:47 +0200

    201106a - xdotool_iron_tab_load_url.sh http://aaa.com  -replace current tab with URL

---
+++ client_sch/zzz_desktop_root_puppy_jon_cfg_dim/usr/local/bin/xdotool_iron_load_url.sh
@@ -0,0 +1,2 @@
+# 201106a - switch to iron , load URL
+wmctrl -a Iron; xdotool key Ctrl+l ; xdotool type "$@"; xdotool key Return;
---
+++ client_sch/zzz_desktop_root_puppy_jon_cfg_dim/usr/local/bin/xdotool_iron_tab_load_url.sh
@@ -0,0 +1,2 @@
+# 201106a - switch to iron , Code org (1.close tab,2.open new tab,3.load code org URL)
+wmctrl -a Iron;xdotool key ctrl+F4 ;xdotool key ctrl+t;xdotool key Ctrl+l ; xdotool type "$@"; xdotool key Return;
